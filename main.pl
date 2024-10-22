64115e072bdba16c23829f9f26b73091d681d82b
samuel <albarran1908@gmail.com>
2024-10-22 09:55:46 -0600

    Update main.pl
    added point 2

--- main.pl
+++ main.pl
@@ -63,9 +63,82 @@ property_extension_clase(Clase, Propiedad, BaseConocimiento, Visitadas, Ext) :-
 % Predicado para verificar si un hecho es miembro de una lista.
 miembro(X, [X|_]).
 miembro(X, [_|T]) :- miembro(X, T).
+
+% punto 2 y 3 predicados
+
+% Leer base de conocimientos desde un archivo .txt
+read_kb(File, KB) :-
+    open(File, read, Stream),
+    read_terms(Stream, KB),
+    close(Stream).
+
+% Leer todos los términos del archivo
+read_terms(Stream, []) :-
+    at_end_of_stream(Stream).
+
+read_terms(Stream, [H|T]) :-
+    \+ at_end_of_stream(Stream),
+    read(Stream, H),
+    read_terms(Stream, T).
+
+% Guardar la base de conocimientos en un archivo .txt
+write_kb(File, KB) :-
+    open(File, write, Stream),
+    write_terms(Stream, KB),
+    close(Stream).
+
+% Escribir todos los términos en el archivo
+write_terms(_, []).
+
+write_terms(Stream, [H|T]) :-
+    write(Stream, H), write(Stream, '.\n'),
+    write_terms(Stream, T).
+
 % punto 2 
 
-%[inserte el código aquí]
+% Añadir una clase a la base de conocimientos y guardarla en un archivo .txt
+add_class(ClassName, ClassMother, CurrentKBFile, NewKBFile) :-
+    read_kb(CurrentKBFile, CurrentKB),
+    append(CurrentKB, [class(ClassName, ClassMother, [], [], [])], NewKB),
+    write_kb(NewKBFile, NewKB).
+
+% Añadir un objeto a la base de conocimientos y guardarlo en un archivo .txt
+add_object(ObjectName, ClassName, CurrentKBFile, NewKBFile) :-
+    read_kb(CurrentKBFile, CurrentKB),
+    append(CurrentKB, [object(ObjectName, ClassName, [])], NewKB),
+    write_kb(NewKBFile, NewKB).
+
+% Añadir una propiedad a una clase existente y guardarla en un archivo .txt
+add_class_property(ClassName, PropertyName, PropertyValue, CurrentKBFile, NewKBFile) :-
+    read_kb(CurrentKBFile, CurrentKB),
+    select(class(ClassName, ClassMother, Properties, Methods, Metadata), CurrentKB, RestKB),
+    NewProperties = [property(PropertyName, PropertyValue) | Properties],
+    append(RestKB, [class(ClassName, ClassMother, NewProperties, Methods, Metadata)], NewKB),
+    write_kb(NewKBFile, NewKB).
+
+% Añadir una propiedad a un objeto existente y guardarla en un archivo .txt
+add_object_property(ObjectName, PropertyName, PropertyValue, CurrentKBFile, NewKBFile) :-
+    read_kb(CurrentKBFile, CurrentKB),
+    select(object(ObjectName, ClassName, Properties), CurrentKB, RestKB),
+    NewProperties = [property(PropertyName, PropertyValue) | Properties],
+    append(RestKB, [object(ObjectName, ClassName, NewProperties)], NewKB),
+    write_kb(NewKBFile, NewKB).
+
+% Añadir una relación a una clase existente y guardarla en un archivo .txt
+add_class_relation(ClassName, RelationName, RelatedClasses, CurrentKBFile, NewKBFile) :-
+    read_kb(CurrentKBFile, CurrentKB),
+    select(class(ClassName, ClassMother, Properties, Methods, Metadata), CurrentKB, RestKB),
+    NewRelations = [relation(RelationName, RelatedClasses) | Metadata],
+    append(RestKB, [class(ClassName, ClassMother, Properties, Methods, NewRelations)], NewKB),
+    write_kb(NewKBFile, NewKB).
+
+% Añadir una relación a un objeto existente y guardarla en un archivo .txt
+add_object_relation(ObjectName, RelationName, RelatedObjects, CurrentKBFile, NewKBFile) :-
+    read_kb(CurrentKBFile, CurrentKB),
+    select(object(ObjectName, ClassName, Properties), CurrentKB, RestKB),
+    NewRelations = [relation(RelationName, RelatedObjects) | Properties],
+    append(RestKB, [object(ObjectName, ClassName, NewRelations)], NewKB),
+    write_kb(NewKBFile, NewKB).
 
 % punto 3
 
